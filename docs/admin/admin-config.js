820dd05c6ec9fea0dd7963c889f47e7754a1010e
ghkdud1987-sys <ghkdud1987@gmail.com>
2026-08-09 05:47:26 +0900

    Publish admin PWA: docs/admin/admin-config.js

---
+++ docs/admin/admin-config.js
@@ -0,0 +1,5 @@
+window.CHY_ADMIN_CONFIG = {
+  appsScriptUrl: "https://script.google.com/macros/s/AKfycbwWCfvg0VraeCjBiUf9o5pSTtbpbxSlqPAJ8qu9VTxfUCE8ZoWorcMZShYZfsZMUND7/exec",
+  oneSignalAppId: "PASTE_ONESIGNAL_APP_ID_HERE",
+  appVersion: "0.45.0-dev"
+};
